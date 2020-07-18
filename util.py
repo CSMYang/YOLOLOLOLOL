e2fb8399e366bae24f8fa963afa20c5b205ac396
apple1 <chengjiahao1234@live.com>
2020-07-18 18:22:29 -0400

    write parse function for config file

---
+++ util.py
@@ -0,0 +1,19 @@
+def get_model_from_config(name):
+    """
+    Get the parameters of each layer of the neural network from the config file based on the given path name.
+    """
+    module_params = []
+    module = open(name, 'r')
+    lines = module.read().split('\n')
+    for line in lines:
+        line = line.strip()
+        if not line or line[0] == '#':
+            continue
+        elif line[0] == '[':
+            layer = dict()
+            layer['type'] = line[1:-1]
+            module_params.append(layer)
+        else:
+            key, value = line.split("=")
+            module_params[-1][key.strip()] = value.strip()
+    return module_params
