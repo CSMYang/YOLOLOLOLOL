c1b18c906980b5f213e051f9fb785f1cc9ccc169
Jimmy <jimmych679@gmail.com>
2020-07-31 22:42:43 -0400

    the format of the vector aren't right

--- DataSet.py
+++ DataSet.py
@@ -28,48 +28,52 @@ class FormatedDataSet(Dataset):
         self.image_boxes = []
 
         label_txt = open(self.label_file, "r")
-        all_entry = label_txt.strip().split(" ")
-        for entry in all_entry:
+        for i, entry in enumerate(label_txt):
+            entry = entry.strip().split(" ")
             pic_name = entry[0]
             self.image.append(image_set_location + "\\" + pic_name)
             entry = entry[1:]
-            number_of_boxes = len(entry)/5
+            number_of_boxes = len(entry)//5
             boxes = []
             labels = []
-            image_x, image_y, _ = cv2.imread(self.image).shape
+            image_x, image_y, _ = cv2.imread(self.image[i]).shape
             for box_index in range(number_of_boxes):
-                xmin = entry[box_index*5 + 5]
-                xmax = entry[box_index*5+1 + 5]
-                ymin = entry[box_index*5+2 + 5]
-                ymax = entry[box_index*5+3 + 5]
-                label = entry[box_index*5+4 + 5]
+                xmin = int(entry[box_index*5])
+                xmax = int(entry[box_index*5+1])
+                ymin = int(entry[box_index*5+2])
+                ymax = int(entry[box_index*5+3])
+                label = int(entry[box_index*5+4])
                 w = xmax-xmin
                 h = ymax-ymin
                 x_center = (w)/2 + xmin
                 y_center = (h)/2 + ymin
                 w /= image_x
                 h /= image_y
-                x /= image_x
-                y /= image y
+                x_center /= image_x
+                y_center /= image_y
                 # [[x,y,w,h], [w1,y1,w1,h1]...] for ith index picture where x y w h are in portion of the image
                 boxes.append([x_center, y_center, w, h])
                 # [l1, l2.......] for ith index picture
                 labels.append(label)
             self.image_boxes.append(boxes)
-            self.label.appen(labels)
+            self.label.append(labels)
+        self.len = i
 
     def __getitem__(self, index):
-        path = self.path[index]
+        path = self.image[index]
         img = cv2.imread(path)
-        box_vector = self.boxes[index]
-        image_label = self.labels[index]
+        box_vector = self.image_boxes[index]
+        image_label = self.label[index]
 
-        tensor_vector = Tensor_Vector(box_vector, image_label)
+        tensor_vector = self.Tensor_Vector(box_vector, image_label)
         img = cv2.resize(img, (self.image_size, self.image_size))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         # do we need to normalize these?
-
+        img = torch.from_numpy(img)
         return img, tensor_vector
+
+    def __len__(self):
+        return self.len
 
     def Tensor_Vector(self, vector, label):
         """ this function takes in a vector in the form of
@@ -80,13 +84,15 @@ class FormatedDataSet(Dataset):
 
         result = torch.zeros(self.grid_size, self.grid_size,
                              5*self.number_bounding_box+self.number_class)
-
+        print(vector, "------------------")
         for index, box_entry in enumerate(vector):
-            x_center = box_entry[0]
-            y_center = box_entry[1]
-            w = box_entry[2]
-            h = box_entry[3]
-            label = vector[index]
+            print(box_entry)
+            x_center = box_entry[index][0]
+            y_center = box_entry[index][1]
+            w = box_entry[index][2]
+            h = box_entry[index][3]
+            print(type(w), h)
+            label = label[index]
             # now we need to determine which box in the grid this center belongs too
             x_center = x_center*418
             y_center = y_center*418
@@ -96,9 +102,11 @@ class FormatedDataSet(Dataset):
             i = math.floor(x_center/cell_size)
             j = math.floor(y_center/cell_size)
             # do we take the abs position or the relative position of x,y wrt to to the cell ij that it is located in
-            for x in range(self.B):
+            for x in range(self.number_bounding_box):
                 k = x*5
+                print(i, j, w, h, 1)
                 result[i, j, k:k+5] = i, j, w, h, 1
-                result[i, j, 5*self.B*5 + label] = 1
+
+                result[i, j, 5*self.number_bounding_box*5 + label] = 1
 
             return result
